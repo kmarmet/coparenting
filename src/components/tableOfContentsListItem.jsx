3c4e563425db245435cf8b63b3ff4b5733c428ae
Kevin Marmet <kmarmet1@gmail.com>
2025-01-27 08:53:52 -0500

    Finished .doc

--- src/components/tableOfContentsListItem.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-
-export default function TableOfContentsListItem({ onClick, text, dataHeader, classes }) {
-  const [formattedText, setFormattedText] = useState(text.replaceAll('-', ' ').toUpperCase())
-  return (
-    <p className={`toc-header ${classes}`} onClick={onClick} data-header-name={dataHeader} dangerouslySetInnerHTML={{ __html: formattedText }}></p>
-  )
-}
